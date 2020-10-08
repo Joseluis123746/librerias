7dff1b93367610ad8af9bfaf146043fcd5f5d868
Joseluis123746 <72416067+Joseluis123746@users.noreply.github.com>
2020-10-08 12:04:20 -0500

    1.0

---
+++ src/main/java/com/mycompany/prueba1/ventana.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.prueba1;
+
+/**
+ *
+ * @author Ghost
+ */
+ import javax.swing.*;
+public class ventana {
+ 
+
+
+public static void main(String[]args){
+Miventana v = new Miventana();
+v.setVisible(true);
+  }    
+
+ 
+}
